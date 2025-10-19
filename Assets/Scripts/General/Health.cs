13d9554742c98d70388ed09fda387f12c8de3b65
Maksim Petukhov <162629927+Rolland-Ultra@users.noreply.github.com>
2025-10-19 11:19:31 +0300

    Изменение полей классов на свойства
    Поля классов были изменены на свойства там, где это возможно; большая часть ненужных теперь методов удалена

--- Assets/Scripts/General/Health.cs
+++ Assets/Scripts/General/Health.cs
@@ -3,9 +3,11 @@ public class Health
 {
     Int_Stat health;
 
-    public Health()
+    public Health() : this(100) { }
+    public Health(uint maxHealth) : this(maxHealth, maxHealth) { }
+    public Health(uint maxHealth, uint currentHealth)
     {
-        health = new Int_Stat(100, 100, 0);
+        health = new Int_Stat((int)maxHealth, (int)currentHealth, 0);
     }
 
     #region INFO
@@ -23,27 +25,27 @@ public class Health
     /// <summary> Увеличивает текущее здоровье на велинину </summary>
     /// <param name="healAmount"> Величина лечения </param>
     public void Heal(uint healAmount) =>
-        health.ChangeStat((int)healAmount);
+        health.Stat += (int)healAmount;
 
     /// <summary> Уменьшает текущее здоровье на велинину </summary>
     /// <param name="damageAmount"> Величина нанесённого урона </param>
     public void TakeDamage(uint damageAmount) =>
-        health.ChangeStat(-(int)damageAmount);
+        health.Stat -= (int)damageAmount;
 
     /// <summary> Увеличивает текущее здоровье до максимума </summary>
     public void FullHeal() =>
-        health.SetStat(health.GetMaxStat());
+        health.Stat = health.MaxStat;
     #endregion
 
     #region MAX_HEALTH
     /// <summary> Изменяет максимальное здоровье на величину </summary>
     /// <param name="maxHealthAmount"> Величина изменения максимального здоровья </param>
     public void ChangeMaxHealth(int maxHealthAmount) =>
-        health.ChangeMaxStat(maxHealthAmount);
+        health.MaxStat += maxHealthAmount;
 
     /// <summary> Устанавливает новое максимальное здоровье </summary>
     /// <param name="maxHealthAmount"> Новое максимальное здоровье </param>
     public void SetMaxHealth(int maxHealthAmount) =>
-        health.SetMaxStat(maxHealthAmount);
+        health.MaxStat = maxHealthAmount;
     #endregion
 }
--- Assets/Scripts/General/Int_Stat.cs
+++ Assets/Scripts/General/Int_Stat.cs
@@ -1,104 +1,80 @@
 ﻿/// <summary> Представляет определённый параметр с максимальным и минимальным значениями </summary>
 public class Int_Stat
 {
+    private int maxStat;
     /// <summary> Максимальное значение параметра </summary>
-    protected int maxStat;
+    public int MaxStat
+    {
+        get { return maxStat; }
+        set
+        {
+            if (value <= MinStat)
+                Error_Handler.ErrorLog(Error_Handler.ErrorCodes.MAX_ERROR);
+            else
+            {
+                maxStat = value;
+                ClampStat();
+            }
+        }
+    }
 
-    /// <summary> Минимальное значение параметра </summary>
-    protected int minStat;
-
+    private int stat;
     /// <summary> Текущее значение параметра </summary>
-    protected int stat;
+    public int Stat
+    {
+        get { return stat; }
+        set
+        {
+            stat = value;
+            ClampStat();
+        }
+    }
+
+    private int minStat;
+    /// <summary> Минимальное значение параметра </summary>
+    public int MinStat
+    {
+        get { return minStat; }
+        set
+        {
+            if (value >= MaxStat)
+                Error_Handler.ErrorLog(Error_Handler.ErrorCodes.MIN_ERROR);
+            else
+            {
+                minStat = value;
+                ClampStat();
+            }
+        }
+    }
 
     public Int_Stat(int maxStat = 100, int stat = 0, int minStat = 0)
     {
-        if (minStat >= maxStat)
-            Error_Handler.ErrorLog(Error_Handler.ErrorCodes.MIN_ERROR);
-        else
-        {
-            this.maxStat = maxStat;
-            this.stat = stat;
-            this.minStat = minStat;
-            ClampStat();
-        }
+        this.minStat = minStat;
+        MaxStat = maxStat;
+        Stat = stat;
     }
 
     /// <summary> Ограничивает текущее значение параметра его максимальным и минимальным значениями </summary>
     public void ClampStat() =>
         stat = System.Math.Clamp(stat, minStat, maxStat);
 
+    /// <summary> Возвращает нормированное текущее значение параметра </summary>
+    /// <returns> Относительное к максимальному и минимальному значение параметра (между 0 и 1) </returns>
+    public float GetNormalizedStat() =>
+        (float)Stat / (MaxStat - MinStat);
+
     #region INFO
     /// <summary> Возвращает информацию о параметре в отформатированном виде </summary>
     /// <returns> Отформатированные данные о параметре </returns>
     public string GetInfo() =>
-        $"{minStat}-->{stat}-->{maxStat}";
+        $"{MinStat}-->{Stat}-->{MaxStat}";
 
     /// <summary> Выводит в консоль отформатированные данные о параметре </summary>
     public void LogInfo() =>
         UnityEngine.Debug.Log(GetInfo());
     #endregion
 
-    #region GETTERS_AND_SETTERS
-    /// <summary> Возвращает текущее значение параметра </summary>
-    /// <returns> Текущее значение параметра </returns>
-    public int GetStat() => stat;
-
-    /// <summary> Возвращает нормированное текущее значение параметра </summary>
-    /// <returns> Относительное к максимальному и минимальному значение параметра (между 0 и 1) </returns>
-    public float GetNormalizedStat() =>
-        (float)stat / (maxStat - minStat);
-
-    /// <summary> Возвращает максимальное значение параметра </summary>
-    /// <returns> Максимальное значение параметра </returns>
-    public int GetMaxStat() => maxStat;
-
-    /// <summary> Возвращает минимальное значение параметра </summary>
-    /// <returns> Минимальное значение параметра </returns>
-    public int GetMinStat() => minStat;
-
-    /// <summary> Устанавливает новое значение параметра </summary>
-    /// <param name="stat"> Новое текущее значение параметра </param>
-    public void SetStat(int stat)
-    {
-        this.stat = stat;
-        ClampStat();
-    }
-
-    /// <summary> Устанавливает новое максимальное значение параметра </summary>
-    /// <param name="maxStat"> Новое максимальное значение параметра </param>
-    public void SetMaxStat(int maxStat)
-    {
-        if (maxStat <= minStat)
-            Error_Handler.ErrorLog(Error_Handler.ErrorCodes.MAX_ERROR);
-        else
-        {
-            this.maxStat = maxStat;
-            ClampStat();
-        }
-    }
-
-    /// <summary> Устанавливает новое минимальноге значение параметра </summary>
-    /// <param name="minStat"> Новое минимальное значение параметра </param>
-    public void SetMinStat(int minStat)
-    {
-        if (minStat >= maxStat)
-            Error_Handler.ErrorLog(Error_Handler.ErrorCodes.MIN_ERROR);
-        else
-        {
-            this.minStat = minStat;
-            ClampStat();
-        }
-    }
-    #endregion
-
     #region CHANGERS
-
-    #region CHANGERS_ONCE
-    /// <summary> Увеличивает текущее значение параметра </summary>
-    /// <param name="difference"> Величина изменения текущего значения параметра </param>
-    public void ChangeStat(int difference) =>
-        ChangeByValue(ref stat, difference);
-
     /// <summary> Умножает текущее значения параметра </summary>
     /// <param name="factor"> Фактор изменения текущего значения параметра </param>
     public void ChangeStat(float factor)
@@ -106,17 +82,7 @@ public class Int_Stat
         if (factor <= 0)
             Error_Handler.ErrorLog(Error_Handler.ErrorCodes.FACTOR_ERROR);
         else
-            ChangeByFactor(ref stat, factor);
-    }
-
-    /// <summary> Увеличивает максимальное значение параметра </summary>
-    /// <param name="difference"> Величина изменения максимального значения параметра </param>
-    public void ChangeMaxStat(int difference)
-    {
-        if (maxStat + difference <= minStat)
-            Error_Handler.ErrorLog(Error_Handler.ErrorCodes.MAX_ERROR);
-        else
-            ChangeByValue(ref maxStat, difference);
+            Stat = (int)(Stat * factor);
     }
 
     /// <summary> Умножает максимальное значения параметра </summary>
@@ -126,17 +92,7 @@ public class Int_Stat
         if (factor <= 0)
             Error_Handler.ErrorLog(Error_Handler.ErrorCodes.FACTOR_ERROR);
         else
-            ChangeByFactor(ref maxStat, factor);
-    }
-
-    /// <summary> Увеличивает минимальное значение параметра </summary>
-    /// <param name="difference"> Величина изменения минимального значения параметра </param>
-    public void ChangeMinStat(int difference)
-    {
-        if (minStat + difference >= maxStat)
-            Error_Handler.ErrorLog(Error_Handler.ErrorCodes.MIN_ERROR);
-        else
-            ChangeByValue(ref minStat, difference);
+            MaxStat = (int)(MaxStat * factor);
     }
 
     /// <summary> Умножает минимальное значения параметра </summary>
@@ -146,27 +102,7 @@ public class Int_Stat
         if (factor <= 0)
             Error_Handler.ErrorLog(Error_Handler.ErrorCodes.FACTOR_ERROR);
         else
-            ChangeByFactor(ref minStat, factor);
+            MinStat = (int)(MinStat * factor);
     }
-
-    /// <summary> Увеличивает переданный параметр на величину </summary>
-    /// <param name="value"> Изменяетмый параметр </param>
-    /// <param name="difference"> Величина изменения параметра </param>
-    private void ChangeByValue(ref int value, int difference)
-    {
-        value += difference;
-        ClampStat();
-    }
-
-    /// <summary> Умножает переданный параметр на фактор </summary>
-    /// <param name="value"> Изменяетмый параметр </param>
-    /// <param name="factor"> Фактор изменения параметра </param>
-    private void ChangeByFactor(ref int value, float factor)
-    {
-        value = (int)(value * factor);
-        ClampStat();
-    }
-    #endregion
-    
     #endregion
 }
