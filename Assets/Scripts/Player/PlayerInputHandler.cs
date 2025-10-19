1b988ae3c1d0cc9a9fc4146d1d1a6a714d9d6a48
Maksim Petukhov <162629927+Rolland-Ultra@users.noreply.github.com>
2025-10-19 08:41:53 +0300

    Система передвижения
    Что сделано
    - Создан класс для обработки пользовательского ввода (нужно будет расширить по ходу разработки);
    - Создан класс для передвижения аватара игрока (нужно будет расширить по ходу разработки);
    - *Префаб персонажа с настроенными двумя новыми классами (в самом проекте обработка пользовательского ввода не должна висеть на персонаже)
    Мотивация
    - Реализация базы для обработки пользовательского ввода при помощи Input Action Assets'ов;
    - Реализация базовой механики передвижения.
    Тестирование
    - Была проверена работа всех методов в среде Unity (на префабе PlayerTest.prefab)
    Необходимо сделать
    - (При необходимости) Добавить реализацию бега (или игрок должен только бегать, а не ходить?)
    Дополнительно
    - Является ли хорошей идеей распределение реализации такого комплексного поведения, как управление аватаром игрока, между несколькими участниками команды без указания конкретных способов реализации частей этой механики?

---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Прикрепляется к отдельному от игрока Gameobject'у
+/// <summary> Обрабатывает поступающий от пользователя ввод </summary>
+public class PlayerInputHandler : MonoBehaviour
+{
+	[Header("Unity Input Asset")]
+	[SerializeField] private InputActionAsset inputActions;
+
+	[Header("Action Map Name Reference")]
+	[SerializeField] private string actionMapName = "Player";
+
+	[Header("Action Name References")]
+	[SerializeField] private string movement = "Run";
+
+	private InputAction moveAction;
+
+	/// <summary> Нормализированный вектор ввода передвижения </summary>
+	[HideInInspector] public Vector2 MovementInput;
+
+	void OnEnable()
+	{
+		inputActions.FindActionMap(actionMapName).Enable();
+	}
+
+	void OnDisable()
+	{
+		inputActions.FindActionMap(actionMapName).Disable();
+	}
+
+	void Awake()
+	{
+		InputActionMap playerInputMap = inputActions.FindActionMap(actionMapName);
+
+		moveAction = playerInputMap.FindAction(movement);
+
+		SubscribeActionValuesToInputEvents();
+	}
+
+	void SubscribeActionValuesToInputEvents()
+	{
+		moveAction.performed += inputInfo => MovementInput = inputInfo.ReadValue<Vector2>();
+		moveAction.canceled += inputInfo => MovementInput = Vector2.zero;
+	}
+}
