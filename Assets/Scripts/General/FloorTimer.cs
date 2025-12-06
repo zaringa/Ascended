f154829e0aab1025215d155ea13e8c649788c5a6
Pavel Gusev <farland271@gmail.com>
2025-12-06 22:20:37 +0300

    Update FloorTimer.cs

--- Assets/Scripts/General/FloorTimer.cs
+++ Assets/Scripts/General/FloorTimer.cs
@@ -1,21 +1,58 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[System.Serializable]
+public class evento
+{
+    [SerializeField]
+
+    public float triggerTime;
+    public string Event_;
+}
 public class FloorTimer : MonoBehaviour
 {
-    // Метод для вызова таймера, который задаёт время и вызываемый метод
-    public Coroutine TimerStart(float duration, Action callback)
+    [Header("General")]
+    public float currentTime = 0F;
+    public List<evento> eventTimeline = new List<evento>();
+    [SerializeField]
+    private float maxTolerance = .4f;
+    [SerializeField]
+    private float maximumTime = 24F;
+    int c__ = 0;
+    // Update is called once per frame
+    void Update()
     {
-        return StartCoroutine(TimerRoutine(duration, callback));
+        HandleTimer();
     }
 
-    // Тело таймера, который по завершении вызывает заданный метод
-    private IEnumerator TimerRoutine(float duration, Action callback)
+    private void HandleTimer()
     {
-        yield return new WaitForSeconds(duration);
-        callback?.Invoke();
+        if(currentTime < maximumTime)
+        {
+            currentTime += Time.deltaTime;
+        }
+        else
+        {
+            
+            while (c__<=0)
+            {
+                FlushTimer();
+                c__ ++;
+            }
+        }
+        if(eventTimeline.Count>0)
+            if(Mathf.Abs(eventTimeline[0].triggerTime-currentTime)< maxTolerance)
+            {
+                Debug.Log(eventTimeline[0].Event_);
+                eventTimeline.RemoveAt(0);
+            }
+
+    }
+
+    private void FlushTimer()
+    {
+        
     }
 }
